bd7376928f86009be7fba0d75c597fd6af37336a
Fabian2199 <neyder.rodriguez@uptc.edu.co>
2021-09-02 10:30:25 -0500

    server index.js

---
+++ Server/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 4000
+var quotes = [];
+getQuote();
+function getQuote(){
+    var Request = require("request");
+    Request.get("https://raw.githubusercontent.com/bitgary/hola-mundo/master/citas.json", (error, response, body) => {
+        if(error) {
+            return console.dir(error);
+        }
+        var data =JSON.parse(body)
+        for (var i = 0; i < data.length; i++) {
+            
+            var quote = data[i].cita
+            quotes.push(quote)
+         }
+    });
+}
+function aleatorio(minimo,maximo){
+    return Math.round(Math.random() * (maximo - minimo) + minimo);
+}
+function quoteRandom(){
+    var index = aleatorio(0,quotes.length);
+    return quotes[index]
+}
+
+app.get('/', (req, res) => {
+    console.log("frase "+ quoteRandom()); 
+    res.send("ok")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
